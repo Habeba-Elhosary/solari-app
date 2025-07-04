bb77a80e20a2c222b61b7c02cff5bd1115b3c835
Habeba-Elhosary <habebaelhosary98@gmail.com>
2025-07-04 12:49:03 +0300

    handle refresh in home screen

--- lib/features/home/presentation/pages/home_screen.dart
+++ lib/features/home/presentation/pages/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:solari/core/constants/app_colors.dart';
 import 'package:solari/core/utils/app_validator/app_validator.dart';
 import 'package:solari/core/widgets/app_spacer.dart';
 import 'package:solari/core/widgets/single_drop_down_selector.dart';
@@ -22,67 +23,80 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
+  void initState() {
+    super.initState();
+    context.read<SystemHomeCubit>().getSystemHomeEvent();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.only(top: 16.sp),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16.sp),
-                  child: Column(
-                    children: [
-                      HomeHeader(),
-                      AppSpacer(heightRatio: 0.7),
-                      BlocListener<AllSystemsCubit, AllSystemsState>(
-                        listenWhen: (prev, curr) => curr is AllSystemsLoaded,
-                        listener: (context, state) {
-                          final systemHomeCubit =
-                              context.read<SystemHomeCubit>();
-                          final allSystemsCubit =
-                              context.read<AllSystemsCubit>();
+      body: RefreshIndicator(
+        color: AppColors.primary,
+        onRefresh: () async {
+          context.read<SystemHomeCubit>().getSystemHomeEvent();
+        },
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.only(top: 16.sp),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.sp),
+                    child: Column(
+                      children: [
+                        HomeHeader(),
+                        AppSpacer(heightRatio: 0.7),
+                        BlocListener<AllSystemsCubit, AllSystemsState>(
+                          listenWhen: (prev, curr) => curr is AllSystemsLoaded,
+                          listener: (context, state) {
+                            final systemHomeCubit =
+                                context.read<SystemHomeCubit>();
+                            final allSystemsCubit =
+                                context.read<AllSystemsCubit>();
 
-                          if (allSystemsCubit.systems.isNotEmpty &&
-                              systemHomeCubit.system == null) {
-                            final firstSystem = allSystemsCubit.systems.first;
-                            systemHomeCubit.selectCompany(entity: firstSystem);
-                          }
-                        },
-                        child: CoreSingleSelectorDropdown<
-                            AllSystemsCubit,
-                            AllSystemsState,
-                            AllSystemsLoading,
-                            AllSystemsError,
-                            System>(
-                          validator: (System? value) =>
-                              Validator.defaultValidator(value?.name),
-                          options: context.watch<AllSystemsCubit>().systems,
-                          onChanged: (System value) {
-                            context
-                                .read<SystemHomeCubit>()
-                                .selectCompany(entity: value);
+                            if (allSystemsCubit.systems.isNotEmpty &&
+                                systemHomeCubit.system == null) {
+                              final firstSystem = allSystemsCubit.systems.first;
+                              systemHomeCubit.selectCompany(
+                                  entity: firstSystem);
+                            }
                           },
-                          label: tr('select_system'),
-                          initState: () {
-                            context
-                                .read<AllSystemsCubit>()
-                                .getAllSystemsEvent();
-                          },
-                          initValue: context.read<SystemHomeCubit>().system,
+                          child: CoreSingleSelectorDropdown<
+                              AllSystemsCubit,
+                              AllSystemsState,
+                              AllSystemsLoading,
+                              AllSystemsError,
+                              System>(
+                            validator: (System? value) =>
+                                Validator.defaultValidator(value?.name),
+                            options: context.watch<AllSystemsCubit>().systems,
+                            onChanged: (System value) {
+                              context
+                                  .read<SystemHomeCubit>()
+                                  .selectCompany(entity: value);
+                            },
+                            label: tr('select_system'),
+                            initState: () {
+                              context
+                                  .read<AllSystemsCubit>()
+                                  .getAllSystemsEvent();
+                            },
+                            initValue: context.read<SystemHomeCubit>().system,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                AppSpacer(heightRatio: 0.7),
-                HomePowerSection(),
-                AppSpacer(heightRatio: 0.7),
-                HomePanelsSection(),
-                AppSpacer(heightRatio: 0.7),
-                HomeTemperatureSection(),
-              ],
+                  AppSpacer(heightRatio: 0.7),
+                  HomePowerSection(),
+                  AppSpacer(heightRatio: 0.7),
+                  HomePanelsSection(),
+                  AppSpacer(heightRatio: 0.7),
+                  HomeTemperatureSection(),
+                ],
+              ),
             ),
           ),
         ),
